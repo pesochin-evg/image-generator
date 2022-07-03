4158f970234e06aee4263e1b8808ac0242bc63dd
Antipascal <pesochin_evgeniy@mail.ru>
2022-07-03 20:48:20 +0300

    remove server

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,13 @@ import (
 	"github.com/Antipascal/image-generator/pkg/bot"
 )
 
-func MainHandler(resp http.ResponseWriter, _ *http.Request) {
-	resp.Write([]byte("Just for heroku"))
-}
+// func MainHandler(resp http.ResponseWriter, _ *http.Request) {
+// 	resp.Write([]byte("Just for heroku"))
+// }
 
 func main() {
-	http.HandleFunc("/", MainHandler)
-	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	// http.HandleFunc("/", MainHandler)
+	// go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
 	bot.Start()
 }
